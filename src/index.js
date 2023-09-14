f18a03d8deef21801b3e43c92b85346dbb49c99a
alessandro baiocco <baioccoalessandro97@gmail.com>
2023-09-14 16:54:36 +0200

    added heart

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import jobs from "./redux/jobs";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
